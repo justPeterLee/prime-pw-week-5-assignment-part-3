4f11f4ac8dbd36f2e7e0e1c08af416203db4f11c
Peter Lee <PeterLee2094@gmail.com>
2022-10-26 15:59:03 -0500

    added/ finished addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,10 @@
 console.log('***** Music Collection *****')
+
+// collection array 
+let collection = [];
+
+
+// addToCollection function 
+function addToCollection(title, artist, yearPublished){
+    collection.push({title: title, artist: artist, yearPublished: yearPublished})
+}
