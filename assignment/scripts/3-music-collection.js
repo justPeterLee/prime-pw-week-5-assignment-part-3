f50579273fe7cfd93b25d7540e9b19c3ab7cc7f4
Peter Lee <PeterLee2094@gmail.com>
2022-10-26 16:03:13 -0500

    tested addToCollection Function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,3 +8,14 @@ let collection = [];
 function addToCollection(title, artist, yearPublished){
     collection.push({title: title, artist: artist, yearPublished: yearPublished})
 }
+
+
+// testing addToCollection function (adding 6 random album)
+addToCollection('song1', 'John Snow', '1999');
+addToCollection('song2', 'Steve Liam', '2020');
+addToCollection('song3', 'Oscar Fisch', '1932');
+addToCollection('song4', 'Tammy Smith', '2002');
+addToCollection('song5', 'Yoda Vader', '2010');
+addToCollection('song6', 'Spongebob', '1997');
+
+console.log(collection)
